2fc6c08ca871b3ebff8f8e6b79426cae02567ffb
Carla <carla.crimi.95@gmail.com>
2025-12-09 17:45:39 -0300

    fix: Corregido error ESM/CommonJS en historias-corazon.js y agregados rewrites para comunidades estres y desempleos

--- api/historias-corazon.js
+++ api/historias-corazon.js
@@ -1,7 +1,7 @@
 // ===== API: HISTORIAS CON CORAZÓN CRESALIA =====
 // Para TODOS los vendedores/emprendedores/servicios que quieren compartir su historia
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Configurar CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
